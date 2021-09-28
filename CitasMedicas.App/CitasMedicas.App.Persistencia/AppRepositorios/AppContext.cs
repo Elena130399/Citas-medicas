8b331db5f3de762e9e9546d0efc251406e435797
MariaELanza <mariaelenarosado@hotmail.com>
2021-09-27 22:12:06 -0500

    commit 2

---
+++ CitasMedicas.App/CitasMedicas.App.Persistencia/AppRepositorios/AppContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using CitasMedicas.App.Dominio;
+
+namespace CitasMedicas.App.Persistencia
+{
+   public class AppContext : DbContext
+   {
+       public DbSet<Persona> Personas {get;set;}
+       public DbSet<Paciente> Pacientes {get;set;}
+       
+       
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+       {
+           if (!optionsBuilder.IsConfigured)
+           {
+               optionsBuilder
+               .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = CitasMedicasData");
+           }
+       }
+   } 
+}
