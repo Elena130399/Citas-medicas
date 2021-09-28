b5ed07263c76aacf3b82cdbcf1b6397dff76c1a4
fedegomez78 <fedebgomez@gmail.com>
2021-09-27 22:53:59 -0500

    "commit 4"

--- CitasMedicas.App/CitasMedicas.App.Dominio/Entidades/Paciente.cs
+++ CitasMedicas.App/CitasMedicas.App.Dominio/Entidades/Paciente.cs
@@ -1,24 +1,13 @@
 using System;
-<<<<<<< HEAD
-=======
-
->>>>>>> 8b331db5f3de762e9e9546d0efc251406e435797
 namespace CitasMedicas.App.Dominio
 {
     public class Paciente:Persona
     {
-<<<<<<< HEAD
         public string TipoPaciente{get;set;}
         public string Historia{get;set;}
         public string Convenio{get;set;}
         public string Direccion{get;set;}
         public string Ciudad{get;set;}
         //comentarios
-=======
-
-        public string Ciudad{get;set;}
-        //falta genero  como se agrega como tipo enum??
-        
->>>>>>> 8b331db5f3de762e9e9546d0efc251406e435797
     }
 }
