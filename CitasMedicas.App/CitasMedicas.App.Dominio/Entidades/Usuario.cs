e7494667bdb539cbc0390955cecdb8e37705f408
MariaELanza <mariaelenarosado@hotmail.com>
2021-10-03 20:03:37 -0500

    Entidad Usuario

---
+++ CitasMedicas.App/CitasMedicas.App.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,9 @@
+using System;
+namespace CitasMedicas.App.Dominio
+{
+    public class Usuario:Persona
+    {
+        public string Login{get;set;}
+        public string contrasena{get;set;}       
+    }
+}
