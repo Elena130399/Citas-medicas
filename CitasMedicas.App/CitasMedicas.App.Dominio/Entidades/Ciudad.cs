e627f16db70b3ace2a35965c6d991c051c40d780
Elena130399 <mariaelenarosado@hotmail.com>
2021-10-14 20:29:13 -0500

    Campos obligatorios 14102021

--- CitasMedicas.App/CitasMedicas.App.Dominio/Entidades/Ciudad.cs
+++ CitasMedicas.App/CitasMedicas.App.Dominio/Entidades/Ciudad.cs
@@ -1,10 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 namespace CitasMedicas.App.Dominio
 {
     public class Ciudad
     {
-        
         public int Id {get;set;}
+        [Required(ErrorMessage ="Nombre de la Ciudad es requerido")]
+        [StringLength(20, ErrorMessage = "La longitud máxima es 20 caracteres") ]
         public string NombreCiudad{get;set;}
     }
 }
